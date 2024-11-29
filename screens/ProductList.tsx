3da1e63ade0e80a9708d1dff2ee389ef99c4caf7
mahmoudElnayeb <mahmoudelnayeb11@gmail.com>
2024-11-29 20:33:24 +0300

    add virtual ist

--- screens/ProductList.tsx
+++ screens/ProductList.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable react-native/no-inline-styles */
-import { FlatList, Platform, StyleSheet, View } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */import { VirtualizedList, StyleSheet, View, Dimensions } from 'react-native';
 import { useEffect, useState } from 'react';
 import Card from '../components/general/Card';
 import Seperator from '../components/general/Seperator';
@@ -10,7 +9,10 @@ import productApis from '../services/products/productsApi.service';
 import AppText from '../components/general/AppText';
 import Button from '../components/general/Button';
 import ActivityIndicator from '../components/general/ActivityIndicator';
-import customDefaultStyle from '../config/styles'
+import customDefaultStyle from '../config/styles';
+
+const ITEM_HEIGHT = 300; // Approximate height of each card item
+
 export default function ProductList({ navigation }: any) {
   const { request: getProducts, data: productListData = [],
     loading,
@@ -22,53 +24,72 @@ export default function ProductList({ navigation }: any) {
 
   useEffect(() => { getProducts() }, []);
 
-const renderData=()=>{
-  if(!error)
-  return <FlatList
-  data={productListData}
-  renderItem={({ item }) => (
+  const getItem = (data: any[], index: number) => {
+    return data[index];
+  };
+
+  const getItemCount = (data: any[]) => {
+    return data.length;
+  };
+
+  const renderItem = ({ item }: any) => (
     <Card
       title={item.title}
       subTitle={item.price + "$"}
       image={item.images.length > 0 && item.images[0].url}
       onPress={() => navigation.navigate("products", { item })}
     />
-  )}
-  keyExtractor={item => item.id.toString()}
-  ItemSeparatorComponent={Seperator}
-  showsVerticalScrollIndicator={false}
-  refreshing={loading}
-  onRefresh={() => {
-    getProducts();
-  }}
-/>
-}
+  );
 
+  const getItemLayout = (_: any, index: number) => ({
+    length: ITEM_HEIGHT,
+    offset: ITEM_HEIGHT * index,
+    index,
+  });
 
-const renderError=()=>{
+  const renderData = () => {
+    if (!error)
+      return <VirtualizedList
+        data={productListData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        getItemCount={getItemCount}
+        getItem={getItem}
+        ItemSeparatorComponent={Seperator}
+        showsVerticalScrollIndicator={false}
+        getItemLayout={getItemLayout}
+        refreshing={loading}
+        onRefresh={() => {
+          getProducts();
+        }}
+        windowSize={1}
+        initialNumToRender={1}
+        maxToRenderPerBatch={1}
+        updateCellsBatchingPeriod={50}
+      />
+  }
+
+  const renderError = () => {
+    return (
+      <View style={{ alignItems: "center" }}>
+        <AppText style={{ fontSize: 20 }}>{errorMessage}</AppText>
+        <Button
+          onPress={() => getProducts()}
+          title="Retry"
+          type="primary"
+          style={{ marginTop: 20 }} />
+      </View>
+    )
+  }
+
   return (
-    <View style={{ alignItems: "center" }}>
-      <AppText style={{ fontSize: 20 }}>{errorMessage}</AppText>
-      <Button
-        onPress={() => getProducts()}
-        title="Retry"
-        type="primary"
-        style={{ marginTop: 20 }} />
-    </View>
-  )
-}
-
-
-
-
-  return (
-    <GradientBackground style={styles.container}  >
+    <GradientBackground style={styles.container} >
       {(error && !loading) && renderError()}
 
       {loading ? (
         <ActivityIndicator visible={true} />
       ) : (
-         renderData()
+        renderData()
       )}
     </GradientBackground>
   );
@@ -78,6 +99,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingVertical: 20,
-    paddingHorizontal:customDefaultStyle.cardPadding.paddingHorizontal
+    paddingHorizontal: customDefaultStyle.cardPadding.paddingHorizontal
   },
 });
